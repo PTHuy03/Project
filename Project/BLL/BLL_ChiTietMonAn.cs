2516bcd8c6785203b7bdc5c77e61100ebd7716ca
PTHuy <pthuy200307@gmail.com>
2023-10-18 18:29:33 +0700

    Project

---
+++ Project/BLL/BLL_ChiTietMonAn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class BUS_ChiTietMonAn
+    {
+        DAO_ChiTietMonAn chiTietMonAn = new DAO_ChiTietMonAn();
+        public DataSet getChiTietMonAn()
+        {
+            return chiTietMonAn.getChiTietMonAn();
+        }
+        public void ThemCongThuc(DTO_ChiTietMonAn ct)
+        {
+            chiTietMonAn.ThemCongThuc(ct);
+        }
+        public void XoaCongThuc(DTO_ChiTietMonAn ct)
+        {
+            chiTietMonAn.XoaCongThuc(ct);
+        }
+    }
+}
